41a09ab9ca79bf1306305a8df6da5ee372326612
mwooddesigns <mwooddesigns@gmail.com>
2015-02-25 10:08:46 -0500

    Commented js and fixed the blurring of the site when the modal fades in.

--- js/main.js
+++ js/main.js
@@ -1,19 +1,20 @@
 $(document).ready(function() {
 
-  var scrollPos = $("body").scrollTop();
+  var scrollPos = $('body').scrollTop();
 
   //Hide Circle nav on page load
-  $("#circle-nav-container").hide();
-  $("#circleNavButtons").hide();
+  $('#circle-nav-container').hide();
+  $('#circleNavButtons').hide();
 
+  //Scroll detection for nav bar transition
   $(window).scroll(function() {
-    scrollPos = $("body").scrollTop();
+    scrollPos = $('body').scrollTop();
     setTimeout(function() {
       if (scrollPos > $('#national').offset().top){
         $('nav').fadeOut('slow');
         $('#circle-nav-container').delay(100).fadeIn('slow');
-        $('.corner').css("z-index", "99");
-        $('#circleNavButtons').css("z-index", "99");
+        $('.corner').css('z-index', '99');
+        $('#circleNavButtons').css('z-index', '99');
       }
 
       else{
@@ -24,51 +25,56 @@ $(document).ready(function() {
     }, 100);
   });
 
-  var navPos=$("body").scrollTop();
+  var navPos=$('body').scrollTop();
   //Controls for circle nav
-  $("#mainBtn").click(function(){
-    navPos=$("body").scrollTop();
-    $("body").scrollTop(navPos);
-    $("#circleNavButtons").fadeToggle("fast", "linear");
-    $("#homeBtn").addClass("bouncy");
-    $("#nationalBtn").addClass("bouncy");
-    $("#globalBtn").addClass("bouncy");
-    $("#aboutBtn").addClass("bouncy");
-    $("#joinBtn").addClass("bouncy");
+  $('#mainBtn').click(function(){
+    navPos=$('body').scrollTop();
+    $('body').scrollTop(navPos);
+    $('#circleNavButtons').fadeToggle('fast', 'linear');
+    $('#homeBtn').addClass('bouncy');
+    $('#nationalBtn').addClass('bouncy');
+    $('#globalBtn').addClass('bouncy');
+    $('#aboutBtn').addClass('bouncy');
+    $('#joinBtn').addClass('bouncy');
     setTimeout(function(){
-      $("body").scrollTop(navPos);
+      $('body').scrollTop(navPos);
     }, 1)
   })
 
+  //Modal window control and button animating
   $('#modal').hide();
   $('.button').mousedown(function(){
     $('.button p')
     .css('font-size', '35px');
   }).mouseup(function(){
     $('.button p').css('font-size', '25px');
-    $('#modal').slideDown("slow");
-    $('#content-container').removeClass("sharp");
-    $('.button').removeClass("sharp");
-    $('#content-container').addClass("blured");
-    $('.button').addClass("blured");
-    $('#roster').addClass("blured");
-    $('.corner').css("z-index", "99");
-    $('#circleNavButtons').css("z-index", "99");
+    $('#modal').fadeIn('slow');
+    $('#content-container').removeClass('sharp');
+    $('.button').removeClass('sharp');
+    $('#roster').removeClass('sharp');
+    $('#content-container').addClass('blured');
+    $('.button').addClass('blured');
+    $('#roster').addClass('blured');
+    $('.corner').css('z-index', '99');
+    $('#circleNavButtons').css('z-index', '99');
   });
 
+  //Control for exiting modal window
   $('.x').click(function() {
-    $('#modal').fadeOut("slow");
-    $('#content-container').removeClass("blured");
-    $('.button').removeClass("blured");
-    $('#content-container').addClass("sharp");
-    $('.button').addClass("sharp");
-    $('#roster').addClass("sharp");
-    $('.corner').css("z-index", "99");
-    $('#circleNavButtons').css("z-index", "99");
+    $('#modal').fadeOut('slow');
+    $('#content-container').removeClass('blured');
+    $('.button').removeClass('blured');
+    $('#roster').removeClass('blured');
+    $('#content-container').addClass('sharp');
+    $('.button').addClass('sharp');
+    $('#roster').addClass('sharp');
+    $('.corner').css('z-index', '99');
+    $('#circleNavButtons').css('z-index', '99');
     $('#about').show();
     $('.about-us').show();
   });
 
+  //Smooth scrolling code snippet from CSS-Tricks
   $(function() {
     $('a[href*=#]:not([href=#])').click(function() {
       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
